06288fba4ca233cdcae7a42b047fb1bb47c4fb7e
keitanxkeitan <keitanxkeitan@gmail.com>
2017-10-24 08:21:36 +0900

    シングルトンクラスを追加。タッチマネージャーファイルだけ追加

---
+++ Assets/Slime/Scripts/Singleton.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Singleton<T> : MonoBehaviour where T : MonoBehaviour {
+
+	protected static T mInstance;
+
+	public static T Instance
+	{
+		get
+		{
+			if( mInstance == null )
+			{
+				mInstance = (T)FindObjectOfType( typeof( T ) );
+
+				if( mInstance == null )
+				{
+					Debug.LogError( "An instance of " + typeof( T ) + " is needed in the scene, but there is none." );
+				}
+			}
+
+			return mInstance;
+		}
+	}
+}
